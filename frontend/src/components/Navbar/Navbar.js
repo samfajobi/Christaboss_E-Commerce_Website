6b97051e63f0c257059b23e59feaf4d97bee496e
Sammy <segzyfaj1@gmail.com>
2022-06-21 15:51:42 +0100

    A new Commit

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import styled from "styled-components"
 import classes from './Navbar.module.css'
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+import ShoppingBasketOutlinedIcon from '@material-ui/icons/ShoppingBasketOutlined';
 
 
 
@@ -12,32 +15,57 @@ const Container = styled.div`
 `
 
 const Wrapper = styled.div`
+ 
 `
 const Right = styled.div`
 `
 
-const Center = styled.div`
+const Icons = styled.div`
 `
 
+const PhoneNo = styled.div``
+
+const Email = styled.div``
+
+
+
+
+
 const Left = styled.div`
+  
 `
+
+const Search = styled.div``
+
+const Cart = styled.div``
+
+const Login = styled.div``
 
 
 const Navbar = () => {
   return (
    <Container>
+    <Wrapper>
       <Right>
+        <Icons>
+          <PhoneNo><PhoneIcon/>+235 08153382359</PhoneNo>
+          <Email>< EmailOutlinedIcon />damilolafolawole@gmail.com</Email>
+        </Icons>
 
       </Right>
-      <Center>
 
-      </Center>
-      <Right>
-        
-      </Right>
+      <Left>
+        <Search>
+        </Search>
+        <Cart>
+        </Cart>
+        <Login>
+        </Login>
+
+      </Left>
 
 
-
+    </Wrapper>
    </Container>
   )
 }
